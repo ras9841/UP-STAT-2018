f789d2179ebb996a5de7f45af059a25b7c2575b7
Roland Sanford <ras9841@rit.edu>
2018-03-26 13:26:45 -0400

    kde: demo of KDE using weather data.

---
+++ src/kde_crime/produceKDE.py
@@ -0,0 +1,8 @@
+# General Libraries
+import numpy as np
+
+# Spark imports
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
+from pyspark.mllib.stat import KernelDensity
+
