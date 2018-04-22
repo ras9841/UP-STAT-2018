78b8e0624fda15e77347ffdf8a194c05120c317b
Roland Sanford <ras9841@rit.edu>
2018-04-22 12:58:45 -0400

    Refactored KDE to be consumable by pipeline.

--- src/kde_crime/spatial_kde.py
+++ src/kde_crime/spatial_kde.py
@@ -18,73 +18,85 @@ def map_to_class(stat_cc):
     else: # Unknown or nan
         return np.NaN
 
+def process_RPD_data(data_loc):
+    # Read in data
+    data = pd.read_csv(data_loc, sep=",")
+    label = "Statute_CrimeCategory"
+    df_xyl = data[["X", "Y"]]
+    df_xyl["class"] = data[label].apply(map_to_class) 
 
-# Read in data
-data = pd.read_csv("../../data/RPD_crime2011toNow.csv", sep=",")
-label = "Statute_CrimeCategory"
-df_xyl = data[["X", "Y"]]
-df_xyl["class"] = data[label].apply(map_to_class) 
+    # Remove rows with NAs
+    null_rows = df_xyl.isnull().any(axis=1)
+    print("Dropping %d rows with null (X,Y,Label) values" %sum(null_rows))
+    df_cleaned_xy = df_xyl[list(map(lambda x: not x, null_rows))]
 
-# Remove rows with NAs
-null_rows = df_xyl.isnull().any(axis=1)
-print("Dropping %d rows with null (X,Y,Label) values" %sum(null_rows))
-df_cleaned_xy = df_xyl[list(map(lambda x: not x, null_rows))]
+    return df_cleaned_xy
 
+def generate_KDE(data, plotDensities=False):
+    # Separate based on class
+    nvc_idx = data.index[data["class"] == CLASS_NVC]
+    vc_idx = data.index[data["class"] == CLASS_VC]
+    df_nvc = data.loc[nvc_idx]
+    df_vc = data.loc[vc_idx]
 
-# Separate based on class
-nvc_idx = df_cleaned_xy.index[df_cleaned_xy["class"] == CLASS_NVC]
-vc_idx = df_cleaned_xy.index[df_cleaned_xy["class"] == CLASS_VC]
-df_nvc = df_cleaned_xy.loc[nvc_idx]
-df_vc = df_cleaned_xy.loc[vc_idx]
-
-# Setup city grid
-extreme_vals = lambda X: (X.min(), X.max())
-xmin, xmax = extreme_vals(data["X"])
-ymin, ymax = extreme_vals(data["Y"])
-xgrid, ygrid = np.mgrid[xmin:xmax:100j, ymin:ymax:100j]
-xygrid = np.vstack([xgrid.ravel(), ygrid.ravel()])
-
-
-# Run KDE on each class
-preds = []
-classes = [df_nvc, df_vc]
-
-for class_df in classes:
-    crime_locs = np.vstack([class_df["X"], class_df["Y"]])
-
-    # Compute KDE (using RBF kernel for now)
-    kde = stats.gaussian_kde(crime_locs)
-
-    # "Predict" on the grid
-    kde_pred = np.reshape(kde(xygrid).T, xgrid.shape)
-    preds.append(kde_pred)
+    # Setup city grid
+    extreme_vals = lambda X: (X.min(), X.max())
+    xmin, xmax = extreme_vals(data["X"])
+    ymin, ymax = extreme_vals(data["Y"])
+    xgrid, ygrid = np.mgrid[xmin:xmax:100j, ymin:ymax:100j]
+    xygrid = np.vstack([xgrid.ravel(), ygrid.ravel()])
 
 
-# Convert to probabilities
-num_NVC = sum(nvc_idx)
-num_VC = sum(vc_idx)
-pi_NVC = num_NVC/(num_VC+num_NVC)
-pi_VC = num_VC/(num_VC+num_NVC)
+    # Run KDE on each class
+    preds = []
+    classes = [df_nvc, df_vc]
 
-p_of_x = np.array(preds[0]*pi_NVC + preds[1]*pi_VC)
-probs = [preds[0]*pi_NVC/p_of_x, preds[1]*pi_VC/p_of_x]
+    for class_df in classes:
+        crime_locs = np.vstack([class_df["X"], class_df["Y"]])
 
-# Plot density estimates
-xvals = np.round(np.linspace(xmin, xmax, 100), 3)
-yvals = np.round(np.linspace(ymin, ymax, 100), 3)
-scale = lambda x: (x - np.min(preds))/(np.max(preds)-np.min(preds))
-scaled_preds = list(map(scale, preds))
-fig, axn = plt.subplots(1, 2, sharex=True, sharey=True)
-names = ["Non-violent Density", "Violent Density"]
-for i, ax, name in zip([0, 1], axn.flat, names):
-    sns.heatmap(scaled_preds[i], ax=ax,            cbar = i == 1,            vmin = 0,            vmax = 1,            cmap = "Reds")
-    ax.set_title(name)
-    ax.set_xticklabels(xvals)
-    ax.set_yticklabels(yvals)
-    ax.get_xaxis().set_minor_locator(mpl.ticker.AutoMinorLocator())
-    ax.get_yaxis().set_minor_locator(mpl.ticker.AutoMinorLocator())
-    ax.grid(b=True, which='major', color='k', alpha=1, linewidth=1.0)
-    ax.grid(b=True, which='minor', color='k', alpha=1, linewidth=0.5)
-fig.tight_layout()
-plt.savefig('output.png', dpi=300)
-plt.show()
+        # Compute KDE (using RBF kernel for now)
+        kde = stats.gaussian_kde(crime_locs)
+
+        # "Predict" on the grid
+        kde_pred = np.reshape(kde(xygrid).T, xgrid.shape)
+        preds.append(kde_pred)
+
+
+    # Convert to probabilities
+    num_NVC = sum(nvc_idx)
+    num_VC = sum(vc_idx)
+    pi_NVC = num_NVC/(num_VC+num_NVC)
+    pi_VC = num_VC/(num_VC+num_NVC)
+
+    p_of_x = np.array(preds[0]*pi_NVC + preds[1]*pi_VC)
+    probs = [preds[0]*pi_NVC/p_of_x, preds[1]*pi_VC/p_of_x]
+
+    if plotDensities:
+        # Plot density estimates
+        xvals = np.round(np.linspace(xmin, xmax, 100), 3)
+        yvals = np.round(np.linspace(ymin, ymax, 100), 3)
+        scale = lambda x: (x - np.min(preds))/(np.max(preds)-np.min(preds))
+        scaled_preds = list(map(scale, preds))
+        fig, axn = plt.subplots(1, 2, sharex=True, sharey=True)
+        names = ["Non-violent Density", "Violent Density"]
+        for i, ax, name in zip([0, 1], axn.flat, names):
+            sns.heatmap(scaled_preds[i], ax=ax, cbar = i == 1, vmin = 0,\
+                    vmax = 1, cmap = "Reds")
+            ax.set_title(name)
+            ax.set_xticklabels(xvals)
+            ax.set_yticklabels(yvals)
+            ax.get_xaxis().set_minor_locator(mpl.ticker.AutoMinorLocator())
+            ax.get_yaxis().set_minor_locator(mpl.ticker.AutoMinorLocator())
+            ax.grid(b=True, which='major', color='k', alpha=1, linewidth=1.0)
+            ax.grid(b=True, which='minor', color='k', alpha=1, linewidth=0.5)
+        fig.tight_layout()
+        plt.savefig('output.png', dpi=300)
+        plt.show()
+
+    return probs
+
+if __name__ == "__main__":
+    data_loc = "../../data/RPD_crime2011toNow.csv"
+    data = process_RPD_data(data_loc)
+    print("Completed Processing.")
+    generate_KDE(data, plotDensities=True)
