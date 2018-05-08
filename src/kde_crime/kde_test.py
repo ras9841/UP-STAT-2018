9727e279c7d4885804e0a7493cfa6dcf7e18bfd3
Roland Sanford <ras9841@rit.edu>
2018-05-08 18:27:24 -0400

    Added stratified test.

--- src/kde_crime/kde_test.py
+++ src/kde_crime/kde_test.py
@@ -12,26 +12,35 @@ X = data[["X", "Y"]]
 
 print("Starting Predictions")
 n_trials = 25
-results = np.zeros(n_trials)
+results = np.zeros([2,n_trials])
 
-for i in range(n_trials):
-    print("\nRunning trial %d/%d"%(i+1, n_trials))
-    # Setup Data 
-    X_tr, X_te, Y_tr, Y_te = train_test_split(X, Y, test_size=0.25)
-    train_df = pd.concat([X_tr, Y_tr], axis=1)
-    y = Y_te.values.reshape(Y_te.shape[0],)
+for test in range(2):
+    print("Running test #%d"%(test+1))
+    for i in range(n_trials):
+        print("\nRunning trial %d/%d"%(i+1, n_trials))
+        # Setup Data 
+        if test == 0:
+            X_tr, X_te, Y_tr, Y_te = train_test_split(X, Y, test_size=0.30)
+        else:
+            X_tr, X_te, Y_tr, Y_te = train_test_split(X, Y, test_size=0.30,\
+                    stratify=Y)
+        train_df = pd.concat([X_tr, Y_tr], axis=1)
+        y = Y_te.values.reshape(Y_te.shape[0],)
 
-    print("Starting KDE")
-    kde = KDE()
-    kde.train(train_df)
-    print("Making predictions")
-    yhat = kde.predict(X_te)
-    results[i] = compute_accuracy(y, yhat)
-    print("Accuracy: %d%%"%(results[i]*100))
+        print("Starting KDE")
+        kde = KDE()
+        kde.train(train_df)
+        print("Making predictions")
+        yhat = kde.predict(X_te)
+        results[test, i] = compute_accuracy(y, yhat)*100
+        print("Accuracy: %d%%"%(results[test,i]))
 
-results = results*100
-print("Accuracy: (%.3f +/- %.3f)%%"%(results.mean(), results.std())
-results_df = pd.DataFrame(results, columns=[""])
+results = results.T
+print("NS Accuracy: (%.3f +/- %.3f)%%"%(results[:,0].mean(),\
+    results[:,0].std()))
+print("STRAT Accuracy: (%.3f +/- %.3f)%%"%(results[:,1].mean(),\
+    results[:,1].std()))
+results_df = pd.DataFrame(results, columns=["Random", "Stratified"])
 results_df.boxplot()
 plt.grid(False)
 plt.ylabel("Accuracy (%)")
